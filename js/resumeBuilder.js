2f8722785d3605bd6720a2e6c6d38ae47ddec66e
Evgeny Degtev <e.a.degtev@gmail.com>
2015-10-07 10:48:39 +0300

    feat: Use .replace and .append methods for my name.
    For the learning purposes.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Evgeny Degtev")
+ /* $("#main").append("Evgeny Degtev"); */
+ 
+ var awesomeThoughts = "My name is Evgeny and I am AWESOME!";
+ 
+ console.log( awesomeThoughts );
+ 
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ 
+ console.log( funThoughts );
+ 
+ $("#main").append( funThoughts );
