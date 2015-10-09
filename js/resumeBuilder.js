7cb011b1dc486d7691981d8980d29ced2de810f9
Evgeny Degtev <e.a.degtev@gmail.com>
2015-10-09 22:39:10 +0300

    feat: Add bio and job sections to the resumeBuilder.js.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,18 +1,19 @@
+//Objects and arrays with data .replace and .append to.
 var work = {
 	"jobs": [
 		{
-			"employer": "bla",
-			"title": "bla",
-			"location": "bla",
-			"dates": "bla",
-			"description": "bla"
+			"employer": "rab1.1",
+			"title": "rab1.2",
+			"location": "rab1.3",
+			"dates": "rab1.4",
+			"description": "rab1.5"
 		},
 		{
-			"employer": "bla",
-			"title": "bla",
-			"location": "bla",
-			"dates": "bla",
-			"description": "bla"
+			"employer": "rab2.1",
+			"title": "rab2.2",
+			"location": "rab2.3",
+			"dates": "rab2.4",
+			"description": "rab2.5"
 		}
 	]
 };
@@ -37,8 +38,14 @@ var projects = {
 var bio = {
 	"name": "bla",
 	"role": "bla",
-	"contacts": {"mobile", "email", "github", "twitter", "location"},
-	"skills": ["bla", "bla"]
+	"contacts": {
+		"mobile": "mobile",
+		"email": "email",
+		"github": "github",
+		"twitter": "twitter",
+		"location": "location"
+		},
+	"skills": ["bla1", "bla2", "bla3", "bla4"]
 };
 
 var education = {
@@ -61,3 +68,29 @@ var education = {
 		}
 	]
 };
+
+//My name.
+$("#header").append(HTMLheaderName.replace("%data%", "Evgeny Degtev"));
+
+//Bio section.
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+}
+
+$("#skills").append(HTMLskills.replace("%data%", bio.skills[0]));
+$("#skills").append(HTMLskills.replace("%data%", bio.skills[1]));
+$("#skills").append(HTMLskills.replace("%data%", bio.skills[2]));
+$("#skills").append(HTMLskills.replace("%data%", bio.skills[3]));
+
+//Jobs section.
+for (var key1 in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+	
+	var FormattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[key1].employer);
+	var FormattedTitle = HTMLworkTitle.replace("%data%", work.jobs[key1].title);
+	
+	$(".work-entry:last").append(FormattedEmployer + FormattedTitle);
+	$(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[key1].dates));
+	$(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[key1].location));
+	$(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[key1].description));
+}
